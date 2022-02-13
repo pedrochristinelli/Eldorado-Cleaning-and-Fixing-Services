52e8c909ae89310546cbfb3bfff0fffa6ff64991
pedrochr <pedrochserra@gmail.com>
2022-02-13 12:58:03 -0300

    Base project generated on Spring Initializr

---
+++ src/test/java/com/eldorado/cleaningservices/CleaningServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eldorado.cleaningservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CleaningServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
